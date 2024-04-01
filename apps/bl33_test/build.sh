6b2475a7a7fea749fed21490a8f893bfa2cee3e3
sk <pi3l4l5q2b@gmail.com>
2024-04-01 15:24:46 +0800

    bl33_test project

---
+++ apps/bl33_test/build.sh
@@ -0,0 +1,3 @@
+CROSS_COMPILE=aarch64-none-elf- cmake -S ../.. -B build -G Ninja --graphviz=build/graph/dep.dot -DPROJECT_PATH:PATH=. \
+-DPROJECT_NAME=bl33_test -DMEM_SCHEME=CUSTOM -DCUSTOM_LINKER_SCRIPT=linker.ld -DCMAKE_BUILD_TYPE=Debug
+cmake --build build
