a42d6ae6ab710986df2c784a1099ad2f4c254963
sk <pi3l4l5q2b@gmail.com>
2024-05-13 14:45:02 +0800

    can't specify the PATH type on command line, which will cause set not able to convert path to absolute

--- apps/gtimer/build.sh
+++ apps/gtimer/build.sh
@@ -1,2 +1,2 @@
-CROSS_COMPILE=aarch64-none-elf- cmake -S ../.. -B build -G Ninja --graphviz=build/graph/dep.dot -DPROJECT_PATH:PATH=. -DPROJECT_NAME=gtimer -DCMAKE_BUILD_TYPE=Debug
+CROSS_COMPILE=aarch64-none-elf- cmake -S ../.. -B build -G Ninja --graphviz=build/graph/dep.dot -DPROJECT_PATH=. -DPROJECT_NAME=gtimer -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
