ef70b7be6eedf2b5a8a27bae9f8742465511087a
sk <pi3l4l5q2b@gmail.com>
2024-05-14 10:20:33 +0800

    rm old cmake files before rebuild all

--- build_all.sh
+++ build_all.sh
@@ -1,2 +1,6 @@
-# find all build.sh in apps/ and run them
+# 1. rm old cmake build files
+find apps/ -maxdepth 3 -type f -name "CMakeCache.txt" -exec rm -rf {} +
+find apps/ -maxdepth 3 -type d -name "CMakeFiles" -exec rm -rf {} +
+
+# 2. find all build.sh in apps/ and run them
 find apps/ -name "build.sh" -execdir sh -c 'if ./{} >/dev/null; then echo "$(pwd)/{} executed successfully"; else echo "\033[1;31mError occurred while executing $(pwd)/{}\033[0m"; exit 1; fi' \;
