efef006634fdbb691cc213874feea16a73d351ea
sk <pi3l4l5q2b@gmail.com>
2024-06-19 17:04:30 +0800

    mv ns_bl1u & fwu_sram to fwu/

--- apps/fwu/fwu_sram/build.sh
+++ apps/fwu/fwu_sram/build.sh
@@ -1,4 +1,4 @@
-CC=aarch64-none-elf-gcc cmake -S ../.. -B build -G Ninja \
+CC=aarch64-none-elf-gcc cmake -S ../../../ -B build -G Ninja \
 --graphviz=build/graph/dep.dot \
 -DPROJECT_NAME=fwu_sram \
 -DPROJECT_PATH=. \
--- apps/fwu/ns_bl1u/build.sh
+++ apps/fwu/ns_bl1u/build.sh
@@ -1,4 +1,4 @@
-CC=aarch64-none-elf-gcc cmake -S ../.. -B build -G Ninja --graphviz=build/graph/dep.dot \
+CC=aarch64-none-elf-gcc cmake -S ../../../ -B build -G Ninja --graphviz=build/graph/dep.dot \
 -DPROJECT_NAME=ns_bl1u -DPROJECT_PATH=. \
 -DCMAKE_BUILD_TYPE=Debug \
 -DMEM_SCHEME=CUSTOM -DCUSTOM_LINKER_SCRIPT=linker.ld
