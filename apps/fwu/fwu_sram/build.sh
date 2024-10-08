5aca4ef536d7f24fa194e76a092664bc4b679393
sk <pi3l4l5q2b@gmail.com>
2024-10-08 16:14:53 +0800

    rhea dram 3G => 384M

--- apps/fwu/fwu_sram/build.sh
+++ apps/fwu/fwu_sram/build.sh
@@ -2,7 +2,6 @@ CC=clang cmake -S ../../../ -B build -G Ninja \
 --graphviz=build/graph/dep.dot \
 -DPROJECT_NAME=fwu_sram \
 -DPROJECT_PATH=. \
--DBOARD=FPGA \
 -DCMAKE_BUILD_TYPE=MinSizeRel \
 -DENABLE_LTO=ON \
 -DMEM_SCHEME=CUSTOM -DCUSTOM_LINKER_SCRIPT=linker.ld
