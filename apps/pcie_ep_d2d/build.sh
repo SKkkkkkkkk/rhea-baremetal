27d45f77add48988f86d57b801c77fbba8a14ac8
pengpeng <pengpeng@seehi.com>
2024-10-25 10:49:41 +0800

    [pcie_ep_d2d] modify build script

--- apps/pcie_ep_d2d/build.sh
+++ apps/pcie_ep_d2d/build.sh
@@ -1,4 +1,4 @@
-ARCH=arm64 CC=/sw/tools/arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-elf/bin/aarch64-none-elf-gcc \
+CC=aarch64-none-elf-gcc \
 cmake -S ../.. -B build -G Ninja \
 --graphviz=build/graph/dep.dot \
 -DPROJECT_NAME=pcie_ep_d2d \
@@ -9,7 +9,7 @@ cmake --build build -t hex && \
 python3 tran2pld.py build/pcie_ep_d2d.hex boot_rom_die0.hex && \
 echo "python3 tran2pld.py build/pcie_ep_d2d.hex  boot_rom_die0.hex"
 
-ARCH=arm64 CC=/sw/tools/arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-elf/bin/aarch64-none-elf-gcc \
+CC=aarch64-none-elf-gcc \
 cmake -S ../.. -B build -G Ninja \
 --graphviz=build/graph/dep.dot \
 -DPROJECT_NAME=pcie_ep_d2d \
