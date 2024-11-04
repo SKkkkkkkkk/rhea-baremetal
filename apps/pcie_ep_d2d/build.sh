4d52dcd44169864174acca21558b27e0b1c965e7
pengpeng <pengpeng@seehi.com>
2024-11-04 15:11:28 +0800

    [pcie_ep_d2d] Output hex file to the build directory

--- apps/pcie_ep_d2d/build.sh
+++ apps/pcie_ep_d2d/build.sh
@@ -7,8 +7,8 @@ cmake -S ../.. -B build -G Ninja \
 -DBOARD=PLD \
 -DSELF_DIE_ID=0
 cmake --build build -t hex && \
-python3 tran2pld.py build/pcie_ep_d2d.hex boot_rom_die0.hex && \
-echo "python3 tran2pld.py build/pcie_ep_d2d.hex  boot_rom_die0.hex"
+python3 tran2pld.py build/pcie_ep_d2d.hex build/boot_rom_die0.hex && \
+echo "python3 tran2pld.py build/pcie_ep_d2d.hex build/boot_rom_die0.hex"
 
 CC=aarch64-none-elf-gcc \
 cmake -S ../.. -B build -G Ninja \
@@ -19,5 +19,5 @@ cmake -S ../.. -B build -G Ninja \
 -DBOARD=PLD \
 -DSELF_DIE_ID=1
 cmake --build build -t hex && \
-python3 tran2pld.py build/pcie_ep_d2d.hex boot_rom_die1.hex && \
-echo "python3 tran2pld.py build/pcie_ep_d2d.hex  boot_rom_die1.hex"
+python3 tran2pld.py build/pcie_ep_d2d.hex build/boot_rom_die1.hex && \
+echo "python3 tran2pld.py build/pcie_ep_d2d.hex build/boot_rom_die1.hex"
